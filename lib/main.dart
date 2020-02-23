2e07ae0c070217ab81012ff03097eeee104f9f58
Helena Upshaw <helenaupshaw2@gmail.com>
2020-02-22 22:30:12 -0500

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,15 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Grocery Share',//title
-      theme: ThemeData (
-        primarySwatch: Colors.blue,
-      ),
-      //What will show up when you open the app?
-      home: MySignIn(),
-    );
-  }//end build
-}//end MyApp
-
-// Define a custom Form widget.
-class MySignIn extends StatefulWidget {
-  @override
-  _MySignInState createState() => _MySignInState();
+void main() {
+  runApp(MaterialApp(
+    title: 'Navigation Basics',
+    home: FirstRoute(),
+  ));
 }
 
-//Define a corresponding State class, which holds
-//the data related in the form
-class _MySignInState extends State<MySignIn> {
+class FirstRoute extends StatelessWidget {
 
+  //Parameters
   final myUsername = TextEditingController();
   final myPassword = TextEditingController();
 
@@ -36,39 +19,52 @@ class _MySignInState extends State<MySignIn> {
     myPassword.dispose();
     super.dispose();
   }
-
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Please enter your username and password'),
-      ),//appBar
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-
+        title: Text('Welcome! Pleas enter your username/password'),
+      ),
+      body: Center(
         child: Column (
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget> [
             TextField (controller: myUsername),
             TextField (controller: myPassword),
-            floatingActionButton: FloatingActionButton (
-              onPressed: () {
-                return showDialog(
-                  context: context,
-                  builder: (context) {
-                  return AlertDialog(
-                    content: Text(myPassword.text), //this will show the text in myPassword
-                  );
-                },//end builder
+          ]
 
-                );//showDialog
-              },//onPressed
-            tooltip: 'Show me the value!',
-            child: Icon(Icons.text_fields),
-            ),//Button
-          ], //end of children list
-        ), //end column
-      ), //end body
-    );//Scaffold
-  } //end build
-}//end home page
+        child: RaisedButton(
+          child: Text('Log in'),
+          onPressed: () {
+            // Navigate to second route when tapped.
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => SecondRoute()),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class SecondRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Second Route"),
+      ),
+      body: Center(
+        child: RaisedButton(
+          onPressed: () {
+            // Navigate back to first route when tapped.
+              Navigator.pop(context);
+          },
+          child: Text('Return to home'),
+        ),
+      ),
+    );
+  }
+}
